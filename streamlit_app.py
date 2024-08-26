08a9455cd22f22b1a54d666e2949d7217da0c7f0
mariam2508 <mariamhossam243@gmail.com>
2024-08-26 22:30:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pickle
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈Diabetes")
+
+Glucose = st.number_input('Glucose', min_value=0.0, max_value=199.0, value=0.0)
+Pregnancies = st.number_input('Pregnancies', min_value=0.0, max_value=17.0, value=0.0)
+BMI = st.number_input('BMI', min_value=0.0, max_value=67.1, value=0.0)
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+output = model.predict([[Glucose, Pregnancies, BMI]])
+st.write("The predicted outcome for Diabetes:", output[0])
